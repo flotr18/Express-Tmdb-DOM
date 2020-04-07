97ac91e22b1addcc8a5baaa71f9fc24c81968ea5
flotr18 <florent.trouillet@edu.devinci.fr>
2020-04-07 20:13:53 +0200

    Comments Added on App.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8000; // Li
 const path = require('path');
 
 
-app.use(express.static('public'));
+app.use(express.static('public')); // Set the repo to use for static files
 
 app.get('/', function(req, res) {
-
-	    res.sendFile(path.join(__dirname + '/index.html'));
-
-
-
-
+	    res.sendFile(path.join(__dirname + '/index.html')); // Display index.html
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(` App listening at http://localhost:${port}`));
